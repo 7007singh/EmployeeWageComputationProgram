213fea512af75bc211151981c29b13c32ca4412e
7007singh <shw12021982@gmail.com>
2023-01-22 23:10:38 +0530

    [Shweta] Added: Part Time Wage Calculation

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -7,14 +7,20 @@ public class EmpWageComputation
         System.out.println("Welcome to Employee Wage Computation Program");
 
         final int FULL_TIME = 1;
+        final int PART_TIME = 2;
         final int EMP_RATE_PER_HR = 20;
         int empHrs = 0, empWage = 0;
         Random random = new Random();
-        int empAttendance = random.nextInt(2);
+        int empAttendance = random.nextInt(3);
         if (empAttendance == FULL_TIME) 
         {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present Full Time");
             empHrs = 8;
+        }
+        else if(empAttendance == PART_TIME)
+        {
+            System.out.println("Employee is Present Part Time");
+            empHrs = 4;
         }
         else
         {
