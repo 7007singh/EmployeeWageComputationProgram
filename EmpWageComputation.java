913dd542ff8feb614feea018d2d3c693a91077c3
7007singh <shw12021982@gmail.com>
2023-01-22 22:58:19 +0530

    [Shweta] Added: Daily Wage Calcualation

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -7,15 +7,20 @@ public class EmpWageComputation
         System.out.println("Welcome to Employee Wage Computation Program");
 
         final int FULL_TIME = 1;
+        final int EMP_RATE_PER_HR = 20;
+        int empHrs = 0, empWage = 0;
         Random random = new Random();
         int empAttendance = random.nextInt(2);
         if (empAttendance == FULL_TIME) 
         {
             System.out.println("Employee is Present");
+            empHrs = 8;
         }
         else
         {
             System.out.println("Employee is Absent");
         }
+        empWage = empHrs * EMP_RATE_PER_HR;
+        System.out.println("Employee Wage:" + empWage);
     }
 }
