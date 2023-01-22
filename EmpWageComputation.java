068f55211eb7f84bb9cac900dbdc2ffa5c02d0ab
7007singh <shw12021982@gmail.com>
2023-01-22 22:44:40 +0530

    [Shweta] Added: Employee Attendance

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,7 +1,21 @@
+import java.util.Random;
+
 public class EmpWageComputation
 {
     public static void main (String args [])
     {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        final int FULL_TIME = 1;
+        Random random = new Random();
+        int empAttendance = random.nextInt(2);
+        if (empAttendance == FULL_TIME) 
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
     }
 }
