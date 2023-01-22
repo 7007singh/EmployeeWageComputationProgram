b8324daa56e2871d73456198b4ae84d9df1faf8a
7007singh <shw12021982@gmail.com>
2023-01-22 23:23:32 +0530

    [Shweta] Refactor: Calculation using Switch Case

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -12,19 +12,20 @@ public class EmpWageComputation
         int empHrs = 0, empWage = 0;
         Random random = new Random();
         int empAttendance = random.nextInt(3);
-        if (empAttendance == FULL_TIME) 
+        switch(empAttendance)
         {
-            System.out.println("Employee is Present Full Time");
-            empHrs = 8;
-        }
-        else if(empAttendance == PART_TIME)
-        {
-            System.out.println("Employee is Present Part Time");
-            empHrs = 4;
-        }
-        else
-        {
-            System.out.println("Employee is Absent");
+            case FULL_TIME:
+               System.out.println("Employee is Present Full Time");
+               empHrs = 8;
+               break;
+            case PART_TIME:
+               System.out.println("Employee is Present Part Time");
+               empHrs = 4;
+               break;
+            default:
+               System.out.println("Employee is Absent");
+               empHrs = 0;
+               break;
         }
         empWage = empHrs * EMP_RATE_PER_HR;
         System.out.println("Employee Wage:" + empWage);
